b5a8049f6f55f69fcd86f01b77f060bcb7a4fc66
Akash Dwivedi <akashdwivedi892@gmail.com>
2025-10-13 17:10:23 +0530

    change in coordinator fetch api

--- api/models/Coordinator.py
+++ api/models/Coordinator.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Coordinator(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(unique=True)
